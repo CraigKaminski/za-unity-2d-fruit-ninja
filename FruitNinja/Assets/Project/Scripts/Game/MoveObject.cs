14a0bcfff7f4caf1b9d6c40cec169e17ff6d995b
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-07 08:20:04 -0400

    Create fruit prefab and script to move objects

---
+++ FruitNinja/Assets/Project/Scripts/Game/MoveObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    [Header("Speed variables")]
+    public float minimumXSpeed;
+    public float maximumXSpeed;
+    public float minimumYSpeed;
+    public float maximumYSpeed;
+
+    [Header("Gameplay")]
+    public float lifetime;
+
+	// Use this for initialization
+	void Start () {
+        // Throw the moveobject upwards.
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(
+            Random.Range(minimumXSpeed, maximumXSpeed),
+            Random.Range(minimumYSpeed, maximumYSpeed)
+        );
+
+        // Wait and destroy.
+        Destroy(gameObject, lifetime);
+	}
+	
+}
