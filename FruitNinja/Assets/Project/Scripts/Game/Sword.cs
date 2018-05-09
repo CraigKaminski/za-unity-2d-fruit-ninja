a1f5e93f7b84c58d6f7da9e7cb5dd260f16bc88b
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-08 21:22:23 -0400

    Add prefab to represent sword cuts and sword object with script to capture mouse swipes

---
+++ FruitNinja/Assets/Project/Scripts/Game/Sword.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+    [Header("Gameplay")]
+    public GameObject cutPrefab;
+    public float cutLifetime;
+
+    private bool dragging;
+    private Vector2 swipeStart;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            dragging = true;
+            swipeStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        } else if (Input.GetMouseButtonUp(0) && dragging)
+        {
+            dragging = false;
+            SpawnCut();
+        }
+	}
+
+    private void SpawnCut()
+    {
+        // Identify where the swipe ended.
+        Vector2 swipeEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Spawnded the cut object.
+        GameObject cutInstance = Instantiate(cutPrefab, swipeStart, Quaternion.identity);
+        cutInstance.GetComponent<LineRenderer>().SetPosition(0, swipeStart);
+        cutInstance.GetComponent<LineRenderer>().SetPosition(1, swipeEnd);
+
+        // Adjusted the edge collider
+        Vector2[] colliderPoints = new Vector2[2];
+        colliderPoints[0] = Vector2.zero;
+        colliderPoints[1] = swipeEnd - swipeStart;
+        cutInstance.GetComponent<EdgeCollider2D>().points = colliderPoints;
+
+        // Scheduled the destruction of teh cut object.
+        Destroy(cutInstance, cutLifetime);
+    }
+}
