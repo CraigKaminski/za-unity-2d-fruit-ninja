d48705fa75a5f62fb01893747e841df2637bcda1
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-08 20:54:33 -0400

    Create object spawner script and use it to repeatedly spawn fruits

---
+++ FruitNinja/Assets/Project/Scripts/Game/ObjectSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    [Header("Targer")]
+    public GameObject prefab;
+
+    [Header("Gameplay")]
+    public float interval;
+    public float minimumX;
+    public float maximumX;
+    public float y;
+
+    [Header("Visuals")]
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", interval, interval);
+	}
+	
+    private void Spawn()
+    {
+        // Instantiate and position the object.
+        GameObject instance = Instantiate(prefab);
+        instance.transform.position = new Vector2(
+            Random.Range(minimumX, maximumX),
+            y
+        );
+
+        // Set a random sprite.
+        Sprite randomSprite = sprites[Random.Range(0, sprites.Length)];
+        instance.GetComponent<SpriteRenderer>().sprite = randomSprite;
+    }
+}
