ea5d791f400e2fac37d0193d8649d5895adc4156
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-30 20:02:40 -0400

    Create script for cuttable objects
    The script will destroy objects on collision with the cut prefab.
    The script will increase the score when collission is with a non-harmful object.

---
+++ FruitNinja/Assets/Project/Scripts/Game/CuttableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttableObject : MonoBehaviour {
+
+    public bool harmful;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Cut")
+        {
+            Destroy(gameObject);
+
+            if (!harmful)
+            {
+                GameObject.Find("ScoreText").transform.GetComponent<ScoreText>().Score += 10;
+            } else
+            {
+
+            }
+        }
+    }
+}
